ec25de1d59339b5982e62d72416a3a80ca623ba9
asantiagoa <47418253+asantiagoa@users.noreply.github.com>
2019-02-07 12:05:02 -0300

    UNIDAD IF

--- 2-InstruccionIf/jsInstruccionIF (4).js
+++ 2-InstruccionIf/jsInstruccionIF (4).js
@@ -1,7 +1,13 @@
 function mostrar()
 {
 //tomo la edad  
-
+var edad;
+edad = document.getElementById("edad").value;
+edad = parseInt(edad);
+if(edad >= 13)&&(17  );
+{
+    alert("es adolescente");
+}
 
 
 }//FIN DE LA FUNCIÓN
