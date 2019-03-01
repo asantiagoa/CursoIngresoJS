5a29fb7129b476a206a067ff776763a66f48cdad
asantiagoa <allemailsalreadytaken@gmail.com>
2019-03-01 12:21:42 -0300

    nice

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/ocho.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/ocho.js
@@ -1,6 +1,6 @@
 function mostrar()
 {
-    var entradas;
+   /* var entradas;
     entradas = 0
     var numero1;
     var numero2;
@@ -35,7 +35,72 @@ function mostrar()
             totalDYR = totalDYR * 1.15;
         }
     }
-    alert("El mayor importe es: "+mayorN+" y el total es: "+totalDYR);
+    alert("El mayor importe es: "+mayorN+" y el total es: "+totalDYR);//ejercicio sfdsh */
 
+
+
+
+  /*2-en ocho.js, .(ejercicio carpeta ###curso de ingreso)
+  Realizar el algoritmo que permita iterar el ingreso de dos datos, un país y la
+  superficie de su territorio (validar) hasta que el usuario quiera e informar al terminar
+  el ingreso por document.write:
+  1-La cantidad de países con superficie impar.
+  2-La cantidad de países con superficie menor a 100
+  3-La cantidad de países con superficie igual a 100
+  4-El nombre del primer país que superó los 100 de superficie
+  5-El promedio de kilómetros ingresados.
+  6-El nombre del que menos territorio tiene.*/
+  var contador = 0;
+  var respuesta = true;//confirmacion
+  var pais;//nombre del pais
+  var superficie;//superficie del país
+  var cantSupImp=0;//cantidad de paises con superficie impar
+  var cantSupMen100=0;//cantidad de paises con superficie menor a 100km
+  var cantSupMay100=0;//cantidad de paises con superficie mayor a 100km
+  var pais1Mayor100;//primer pais ingresado cuya superficie supera los 100km
+  var promKM;//promedio de KM's de tierra
+  var menorPais;//nombre del pais con menor superficie
+  var supMin;//la menor superficie
+  var supTotal=0;//la suma de todas las superficies
+  while(respuesta==true)
+  {
+    pais = prompt("ingrese el nombre del país");
+    superficie= parseInt(prompt("ingrese la superficie del pais"));
+    while(superficie<0||isNaN(superficie))
+    {
+      superficie= parseInt(prompt("dato invalido, ingrese la superficie del pais"));
+    }
+    if((superficie%2)!=0)//si la superficie es impar suma +1 a la cantidad de paises con sup. impar
+    {
+      cantSupImp++;
+    }
+    if(superficie>100)//si la superficie es mayor a 100 suma +1 a la cantidad de paises con sup>100
+    {
+      cantSupMay100++;
+    }
+    else//si la superficie no es mayor a 100 suma +1 a la cantidad de paises con sup<100
+    {
+      cantSupMen100++;
+    }
+    if(cantSupMay100==1)//si es el primer pais que se ingresa y tiene mas de 100KM de sup. su nombre se guarda
+    {
+      pais1Mayor100 = pais;
+    }
+    if(contador==0||superficie<supMin)//si es la primera vez que se ingresan datos o si la superficie del pais es la menor ingresada hasta ahora se guarda su nombre
+    {
+      supMin=superficie;
+      menorPais = pais;
+    }
+    contador++;
+    supTotal = supTotal + superficie;
+    promKM = supTotal/contador
+    respuesta = confirm("seguir ingresando?");
+  }
+  document.write( cantSupImp+" paises tienen superficie impar<br>");//1)
+  document.write(cantSupMen100+" tienen superficie menor a 100KM<br>");//2)
+  document.write(cantSupMay100+" tienen superficie mayor a 100KM<br>");//3)
+  document.write(pais1Mayor100+" es el primer pais ingresado en superar los 100KM<br>");//4)
+  document.write(promKM+" es el promedio de las superficies ingresadas<br>");//5)
+  document.write(menorPais+" es el pais mas chico<br>");//6)
 
 }
--- 9-Parciales/2018_1Cuatri_Recu_fuentes/siete.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/siete.js
@@ -1,4 +1,115 @@
 function mostrar()
 {
+  /*var contador = 0;//punto del pdf de recuperatorio.
+  var notaI;//nota ingresada
+  var notamin;//menor nota
+  var nota=0;//total de notas
+  var notaP;//promedio de las notas
+  var cVar186=0;//cant. de varones con nota 6+ mayores a 18
+  var nota1M;//nota de la primera mujer
+  var notaJ;//nota del mas joven
+  var edad;//edad ingresada
+  var edad1M;//edad de la primera mujer ingresada
+  var edadMin;//edad menor
+  var sexo;//sexo ingresado
+  var sexomin;//sexo de la nota minima
+  var sexoJ;//sexo del mas joven
+  var contM=0;//contador de mujeres
+  while(contador<5)
+  {
+    notaI = parseInt(prompt("Ingrese nota"));//ingreso nota
+    while(notaI>10||notaI<0||isNaN(notaI))//confirmo nota
+    {
+      notaI = parseInt(prompt("numero invalido,Ingrese nota"));
+    }
+    sexo = prompt("Ingrese sexo (f o m)");//ingreso sexo
+    while(sexo !="f" && sexo !="m")//confirmo sexo
+    {
+      sexo = prompt("entrada invalida, Ingrese sexo (f o m)");
+    }
+    if(sexo=="f")//contador de mujeres
+    {
+      contM++;
+    }
+    edad = parseInt(prompt("ingrese una edad"));//ingreso edad
+    if(edad>=18 && notaI>=6 && sexo=="m")//si tiene 18 o mas, si saco 6 o mas y si es hombre
+    {
+      cVar186++;
+    }
+    if(edad<edadMin||contador==0)//consigo edad menor y le atribuyo el sexo y la nota
+    {
+      edadMin=edad;
+      notaJ = notaI;
+      sexoJ = sexo;
+    }
+    if(contM==1)//si es la primera mujer
+    {
+      edad1M = edad;
+      nota1M = notaI;
+    }
+    if (notaI<notamin||contador==0)//consigo menor nota
+    {
+      notamin = notaI;
+      sexomin = sexo;
+    }
+    contador++;
+    nota = nota+notaI;//total de notas
+  }
 
+  notaP = nota/contador;//promedio de notas
+  alert("el promedio de las notas es: "+notaP);//A)
+  alert("la nota mas baja es: "+notamin+" y el sexo de quien la obtuvo es "+sexomin);//B)
+  alert(cVar186+" varones mayores de 18 años consiguieron una nota mayor o igual a 6");//C)
+  alert("la persona mas joven obtuvo un "+notaJ+" y es de sexo "+sexoJ);//D)
+  if(contM==0)
+  {
+    alert("no hay ninguna mujer");
+  }
+  else
+  {
+    alert("la primera mujer consiguió un "+nota1M+" de nota y tiene "+edad1M+" años");
+  }*/
+  var contador=0;//punto de material ejercicio 1
+  var velocidadIngresada;//velocidad ingresada
+  var sumVel=0;//velocidad total
+  var velProm;//velocidad promedio
+  var velMin;//velocidad minima
+  var velMaxSol;//velocidad mas alta de combustible solido
+  var combustible;//tipo de combustible
+  var cCombLiq=0;//cantidad de vehiculos con combustible liquido
+  var tcombMin;//tipo de combustible velocidad minima
+  while(contador<5)
+  {
+    velocidadIngresada=parseInt(prompt("ingrese una velocidad (de 0 a 250 km/h)"));
+    while(velocidadIngresada>250 || velocidadIngresada<0||isNaN(velocidadIngresada))
+    {
+      velocidadIngresada=parseInt(prompt("valor invalido, ingrese una velocidad (de 0 a 250 km/h)"));
+    }
+    combustible = prompt("ingrese el tipo de combustible del vehiculo (s o l)")
+    while(combustible != "s"&& combustible != "l")
+    {
+      combustible = prompt("valor invalido, ingrese el tipo de combustible del vehiculo (s o l)")
+    }
+    sumVel = sumVel + velocidadIngresada;
+    if((combustible=="s")&&(contador==0||velocidadIngresada>velMaxSol))
+    {
+      velMaxSol=velocidadIngresada;
+      console.log(velMaxSol);
+    }
+    if(contador==0||velocidadIngresada<velMin)
+    {
+      velMin = velocidadIngresada;
+      tcombMin = combustible;
+    }
+    if(combustible =="l"&& velocidadIngresada >100)
+    {
+      cCombLiq++;
+    }
+    contador++;
+  }
+  velProm = sumVel / contador;
+  alert("el promedio de velocidades es "+velProm);
+  alert("la velocidad mas baja es "+velMin+" y el tipo de combustible "+tcombMin);
+  alert("la cantidad de autos que van a mas de 100km/h y usan combustible liquido es "+cCombLiq);
+  alert("el auto con combustible solido de mayor velocidad llega a los "+velMaxSol+"km/h");
 }
